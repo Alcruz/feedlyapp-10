1ccb6b63fb92999e8701fa8351ecc9b2d7f4fb1e
Alvin De la Cruz <alvinj.delacruz@gmail.com>
2017-01-11 01:07:54 -0400

    Add SignIn Enable and Disable when email account and password change

---
+++ App.UnitTests/SignInViewModelTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using App.ViewModels;
+using Xunit;
+
+namespace App.UnitTests
+{
+    public class SignInViewModelTest
+    {
+        [Fact]
+        public void SignInCommand_IsActivateWhenEmailAccountAndPasswordAreNotNullOrEmpty()
+        {
+            var signInViewModel = new SignInViewModel();
+            signInViewModel.EmailAccount = "johndow@domain.com";
+            signInViewModel.Password = "123abc";
+            
+            Assert.True(signInViewModel.SignInCommand.CanExecute(null));
+        }
+
+        [Fact]
+        public void SignInCommand_RaisCanExecuteChangeEventWhenEmailAccountAndPasswordChange()
+        {
+            var signInViewModel = new SignInViewModel();
+            bool commandHasNotified = false;
+
+            signInViewModel.SignInCommand.CanExecuteChanged += (sender, args) => commandHasNotified = true;
+            signInViewModel.EmailAccount = "johndow@domain.com";
+            signInViewModel.Password = "123abc";
+            
+            Assert.True(commandHasNotified);
+        }
+
+
+        [Fact]
+        public void SignIn_EmailAccountPropertyIsNotNullWhenSignIn()
+        {
+            var signInViewModel = new SignInViewModel();
+            signInViewModel.Password = "";
+            Assert.ThrowsAny<ArgumentNullException>(() => signInViewModel.SignIn());
+        }
+
+        [Fact]
+        public void SignIn_PasswordPropertyIsNotNullWhenSignIn()
+        {
+            var signInViewModel = new SignInViewModel();
+            signInViewModel.EmailAccount = "";
+            Assert.ThrowsAny<ArgumentNullException>(() => signInViewModel.SignIn());
+        }
+    }
+}
--- App/ViewModels/SignInViewModel.cs
+++ App/ViewModels/SignInViewModel.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using App.Utils;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace App.ViewModels
 {
     public class SignInViewModel : ViewModelBase
     {
-        public string EmailAccount { get; set; }
-        public string Password { get; set; }
-        
+        private string _emailAccount;
+        private string _password;
+
+        public string EmailAccount
+        {
+            get { return _emailAccount; }
+            set
+            {
+                Set(ref _emailAccount, value);
+                SignInCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                Set(ref _password, value);
+                SignInCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public RelayCommand SignInCommand { get; set; }
+
+        public SignInViewModel()
+        {
+            SignInCommand = new RelayCommand(async () => await SignIn(), IsSignInCommandEnabled);
+        }
+
+        private bool IsSignInCommandEnabled() =>
+            !string.IsNullOrEmpty(EmailAccount) && !string.IsNullOrEmpty(Password);
+
         public Task SignIn()
         {
             Assert.IsNotNull(EmailAccount, nameof(EmailAccount));
