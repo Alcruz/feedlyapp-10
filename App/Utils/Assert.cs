11b0f95976d054274af547fc3e9e83b1b53ae4ef
Alvin De la Cruz <alvinj.delacruz@gmail.com>
2017-01-11 00:25:56 -0400

    Add project files.

---
+++ App/Utils/Assert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Utils
+{
+    public class Assert
+    {
+        public static void IsNotNull<T>(T param, string paramName)
+        {
+            if (param == null)
+                throw new ArgumentNullException(paramName);
+        }
+    }
+}
---
+++ App/ViewModels/SignInViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Utils;
+using GalaSoft.MvvmLight;
+
+namespace App.ViewModels
+{
+    public class SignInViewModel : ViewModelBase
+    {
+        public string EmailAccount { get; set; }
+        public string Password { get; set; }
+        
+        public Task SignIn()
+        {
+            Assert.IsNotNull(EmailAccount, nameof(EmailAccount));
+            Assert.IsNotNull(Password, nameof(Password));
+
+            return Task.Delay(1000);
+        }
+    }
+}
