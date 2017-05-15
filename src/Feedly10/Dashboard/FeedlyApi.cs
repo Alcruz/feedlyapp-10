bccead16882cbf00723d202b47539ec997298821
Alvin Jonel de la Cruz <alvinj.delacruz@outlook.com>
2017-05-14 21:07:15 -0400

    Updating Nuget Packages. Also Install System.ValueTuple.

--- src/Feedly10/Dashboard/FeedlyApi.cs
+++ src/Feedly10/Dashboard/FeedlyApi.cs
@@ -9,71 +9,64 @@ using Newtonsoft.Json.Serialization;
 
 namespace App.Dashboard
 {
-    public class FeedlyApi
-    {
-        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
-        {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new SnakeCaseNamingStrategy()
-            }
-        };
+	public class FeedlyApi
+	{
+		public FeedlyApi(OAuthToken oAuthToken)
+		{
+			BaseUri = "https://sandbox.feedly.com/v3";
+			OAuthToken = oAuthToken;
+		}
 
-        public string BaseUri { get; set; }
-        public OAuthToken OAuthToken { get; set; }
-        public JsonSerializer JsonSerializer { get; set; }
+		public string BaseUri { get; }
+		public OAuthToken OAuthToken { get; }
+		public JsonSerializer JsonSerializer { get; }
 
-        private string JsonSerialize<TModel>(TModel model) =>
-            JsonConvert.SerializeObject(model, _jsonSerializerSettings);
+		private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+		{
+			ContractResolver = new DefaultContractResolver
+			{
+				NamingStrategy = new SnakeCaseNamingStrategy()
+			}
+		};
 
-        private TModel JsonDeserialize<TModel>(string json) =>
-            JsonConvert.DeserializeObject<TModel>(json, _jsonSerializerSettings);
-
-        public FeedlyApi(OAuthToken oAuthToken)
-        {
-            BaseUri = "https://sandbox.feedly.com/v3";
-            OAuthToken = oAuthToken;
-
-        }
-
-        public async Task<List<Category>> GetCategories()
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var request = new HttpRequestMessage(HttpMethod.Get, new Uri(BaseUri + "/categories"));
-                request.Headers.TryAppendWithoutValidation("Authorization", $"OAuth {OAuthToken.AccessToken}");
-                var response = await httpClient.SendRequestAsync(request);
-                var categoriesJson = await response.Content.ReadAsStringAsync();
-                var categories = JsonDeserialize<List<Category>>(categoriesJson);
-                return categories;
-            }
-        }
-
-        public async Task<List<Subscrition>> GetSubscrition()
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var request = new HttpRequestMessage(HttpMethod.Get, new Uri(BaseUri + "/subscriptions"));
-                request.Headers.TryAppendWithoutValidation("Authorization", $"OAuth {OAuthToken.AccessToken}");
-                var response = await httpClient.SendRequestAsync(request);
-                var subscriptionJson = await response.Content.ReadAsStringAsync();
-                var subscrition = JsonDeserialize<List<Subscrition>>(subscriptionJson);
-                return subscrition;
-            }
-        }
-
-        public async Task<StreamDto> GetContent(string streamId)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var request = new HttpRequestMessage(HttpMethod.Get,
-                    new Uri(BaseUri + $"/streams/{Uri.EscapeDataString(streamId)}/contents"));
-                request.Headers.TryAppendWithoutValidation("Authorization", $"OAuth {OAuthToken.AccessToken}");
-                var response = await httpClient.SendRequestAsync(request);
-                var contentStreamJson = await response.Content.ReadAsStringAsync();
-                var contentStream = JsonDeserialize<StreamDto>(contentStreamJson);
-                return contentStream;
-            }
-        }
-    }
+		public async Task<List<Category>> GetCategories()
+		{
+			using (var httpClient = new HttpClient())
+			{
+				var request = new HttpRequestMessage(HttpMethod.Get, new Uri(BaseUri + "/categories"));
+				request.Headers.TryAppendWithoutValidation("Authorization", $"OAuth {OAuthToken.AccessToken}");
+				var response = await httpClient.SendRequestAsync(request);
+				var categoriesJson = await response.Content.ReadAsStringAsync();
+				var categories = JsonDeserialize<List<Category>>(categoriesJson);
+				return categories;
+			}
+		}
+		public async Task<List<Subscrition>> GetSubscrition()
+		{
+			using (var httpClient = new HttpClient())
+			{
+				var request = new HttpRequestMessage(HttpMethod.Get, new Uri(BaseUri + "/subscriptions"));
+				request.Headers.TryAppendWithoutValidation("Authorization", $"OAuth {OAuthToken.AccessToken}");
+				var response = await httpClient.SendRequestAsync(request);
+				var subscriptionJson = await response.Content.ReadAsStringAsync();
+				var subscrition = JsonDeserialize<List<Subscrition>>(subscriptionJson);
+				return subscrition;
+			}
+		}
+		public async Task<StreamDto> GetContent(string streamId)
+		{
+			using (var httpClient = new HttpClient())
+			{
+				var request = new HttpRequestMessage(HttpMethod.Get,
+					new Uri(BaseUri + $"/streams/{Uri.EscapeDataString(streamId)}/contents"));
+				request.Headers.TryAppendWithoutValidation("Authorization", $"OAuth {OAuthToken.AccessToken}");
+				var response = await httpClient.SendRequestAsync(request);
+				var contentStreamJson = await response.Content.ReadAsStringAsync();
+				var contentStream = JsonDeserialize<StreamDto>(contentStreamJson);
+				return contentStream;
+			}
+		}
+		private string JsonSerialize<TModel>(TModel model) => JsonConvert.SerializeObject(model, _jsonSerializerSettings);
+		private TModel JsonDeserialize<TModel>(string json) => JsonConvert.DeserializeObject<TModel>(json, _jsonSerializerSettings);
+	}
 }
